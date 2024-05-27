5608c9362cb12af03a49fc164c8ca9edf62a4e12
LUCAS CARDOSO BELOUS <lucas_belous@estudante.sesisenai.org.br>
2024-05-27 12:05:10 -0300

    comentar script de userManager

--- scripts/userManager.js
+++ scripts/userManager.js
@@ -1,23 +1,32 @@
 var usuarios = []
 
+
+// Cadastrar um novo usuário
+// TODO: conexao com banco de dados. ( no momento esta apenas cadastrando usuario em runtime )
 function register() {
     let nome = document.getElementById("nome").value;
     let email = document.getElementById("email").value;
     let senha = document.getElementById("senha").value;
     let repetirSenha = document.getElementById("repetir-senha").value;
     
+    //Confirmar senha
     if (senha != repetirSenha) {
+        //Confirmacao de senha nao corresponde a senha original
         alert("Confirme sua senha.")
         return;
     }
 
     for (let i = 0; i < usuarios.length; i++) {
+        // Loop em cada usuario cadastrado
         if (usuarios[i].email == email || usuarios[i].nome == nome) {
+            // Se tiver alguem com o mesmo email e o mesmo nome, nao continuar
             alert("Usuario ja cadastrado.")
             return;
         }
     }
 
+    // Se chegamos aqui e porque passamos por todas as validacoes
+    // E assim podemos criar um novo usuario
     usuarios.push({nome: nome, email: email, senha: senha})
 }
 
@@ -26,11 +35,15 @@ function login() {
     let senha = document.getElementById("senha").value;
 
     for (let i = 0; i < usuarios.length; i++) {
+        // Loop em todos os usuarios
         if (usuarios[i].email == nome && usuarios[i].senha == senha) {
+            // Se um usuario corresponde ao login informado, entao deu sucesso
             alert("Login com sucesso.")
             return;
         }
     }
+
+    // Se chegamos aqui e porque nenhum usuario corresponse ao login informado
     alert("Usuario nao encontrado.")
 }
 
@@ -38,4 +51,5 @@ function loadUsers() {
     usuarios.push({nome: "usr", email: "email@email.com", senha: "email"})
 }
 
+// Carregar um usuario mockado para testes
 loadUsers()

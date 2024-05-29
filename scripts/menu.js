9619edb916e7b5a711bf0656a7ec3034d4fb39ec
CAUÃ HENRIQUE BENIGRI GONÇALVES <caua_goncalves@estudante.sesisenai.org.br>
2024-05-29 12:30:15 -0300

    Implementação Menu

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,10 +1,7 @@
 menu = document.getElementById("menu-icon")
 dropdown = document.getElementById("dropdown")
-
 var flag_open = false
-
 menu.addEventListener("click", function (e) {
-    
     if (flag_open == false){
         menu.style.cssText = `transition: 1s; 
                             -webkit-transform: rotate(90deg); 
@@ -12,39 +9,38 @@ menu.addEventListener("click", function (e) {
                             -o-transform: rotate(90deg); 
                             -ms-transform: rotate(90deg); 
                             transform: rotate(90deg);`
-                            
         dropdown.innerHTML = `
-            <div class="option"><a href="./initial-series.html">Séries Iniciais</a></div>
-            <div class="option"><a href="./highschool.html">Ensino Médio</a></div>
+            <div class="option"> <a href="./login.html">Login</a></div>
+            <div class="option"> <a href="./register.html">Cadastro</a></div>
+            <div class="option"> <a href="./initial-series.html">Séries Iniciais</a></div>
+            <div class="option"> <a href="./final-series.html">Séries Finais</a></div>
+            <div class="option"> <a href="./highschool.html">Ensino Médio</a></div>
         `
-
         build_menu()
-
-        dropdown.style.cssText = `transition: 1s ;
-                                border-left: 2px solid white;
-                                border-right: 2px solid white;
+        dropdown.style.cssText = `
+                                transition: 1s ;
                                 position: fixed; 
                                 right: 0; 
-                                width: 33.33%; 
+                                width: 36%; 
                                 background-color: #093774; 
                                 box-shadow: rgba(0, 0, 0, 0.5) 0px 3px 8px;`
         flag_open = true
     }else{
         menu.style.cssText = `transition: 1s;`
-        dropdown.style.cssText = `transition: 1s; visibility: hidden;`
-
+        dropdown.style.cssText = `transition: 1s;`
         dropdown.innerHTML = ``
-        
         flag_open = false
     }
 })
 
 function build_menu(){
     options = document.getElementsByClassName("option")
-    
     for (let i = 0; i < options.length; i++) {
         options[i].style.cssText = `
-        border-bottom: 2px solid white;
+        transition: 1s;
+        border-left: 1px solid white;
+        border-right: 1px solid white;
+        border-bottom: 1px solid white;
         width: 100%;
         display: flex;
         align-items: center;
